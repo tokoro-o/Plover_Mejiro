f29ad737b131f692a2b8f2fd91381cdb7c69e39e
JEEBIS27 <144299154+JEEBIS27@users.noreply.github.com>
2025-02-14 16:09:49 +0900

    Add files via upload

---
+++ plover_Sokutaipu_EX/system.py
@@ -0,0 +1,71 @@
+KEYS = (
+'*-', 'Y-', 'T-', 'H-', 'K-', 'S-', 'A-', 'I-', 'O-', 'X-', 't-', 'k-', 'n-',
+'#',
+'-*', '-Y', '-T', '-H', '-K', '-S', '-A', '-I', '-O', '-X', '-t', '-k', '-n',
+'-1','-2','-3','-4','-5',
+'-6','-7','-8','-9','-0',
+)
+
+IMPLICIT_HYPHEN_KEYS = ('#')
+
+SUFFIX_KEYS = ()
+
+NUMBER_KEY = None
+
+NUMBERS = {}
+
+UNDO_STROKE_STENO = ('*-')
+
+ORTHOGRAPHY_RULES = []
+
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+ORTHOGRAPHY_WORDLIST = None
+
+KEYMAPS = {
+        'Keyboard': {
+        '*-' : 'q',
+        'Y-' : 'a',
+        'T-' : 's',
+        'H-' : 'w',
+        'K-' : 'd',
+        'S-' : 'e',
+        'A-' : 'f',
+        'I-' : 'r',
+        'O-' : 'g',
+        'X-' : 't',
+        't-' : 'c',
+        'k-' : 'v',
+        'n-' : 'space',
+        '#'  : 'b',
+        '-*' : ('p','[',']'),
+        '-Y' : ';',
+        '-T' : 'l',
+        '-H' : 'o',
+        '-K' : 'k',
+        '-S' : 'i',
+        '-A' : 'j',
+        '-I' : 'u',
+        '-O' : 'h',
+        '-X' : 'y',
+        '-t' : 'm',
+        '-k' : 'n',
+        '-n' : 'enter',
+        '-1' : '1',
+        '-2' : '2',
+        '-3' : '3',
+        '-4' : '4',
+        '-5' : '5',
+        '-6' : '6',
+        '-7' : '7',
+        '-8' : '8',
+        '-9' : '9',
+        '-0' : '0',
+        'arpeggiate' : (',','.'),
+        'no-op' : '\''
+        }
+}
+
+
+DICTIONARIES_ROOT = 'asset:plover_Sokutaipu_EX:dictionaries'
+DEFAULT_DICTIONARIES = ('Plover_Sokutaipu_EV_.py','abbreviation_for_Sokutaipu_EX.json','commands_for_Sokutaipu_EX.json')
